64ed540eea103eb9bb4a5cf8b9d4cef7a5f666e1
Mohd Shad <shadmsshad@gmail.com>
2022-04-24 14:47:01 -1200

    Add files via upload

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2021-2022 by shad @ github , < https://github.com/Shaddoar/Akena-Musix.git >.
+#
+# This file is part of < https://github.com/Shaddor/Akena musix > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/shaddoar/akena musix Bot/blob/master/LICENSE >
+#
+# All rights reserved.
+
+
+from .config import *
